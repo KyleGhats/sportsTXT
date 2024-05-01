94bf8c116f84dd88e7c9a2696525976947ec0bd3
Kyle Gatudan <tatsumakigei@gmail.com>
2024-05-01 18:17:17 +0800

    header nav and white color changed

--- src/layouts/topnav.php
+++ src/layouts/topnav.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" aria-label="Fourth navbar example">
 	<div class="container">
-		<a class="navbar-brand" href="#">Expand at md</a>
+		<a class="navbar-brand" href="#">
+		<img src="./assets/img/logo/logo.avif" class="d-block mx-lg-auto img-fluid" alt="Football Field" loading="lazy">
+		</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -11,23 +13,16 @@
 					<a class="nav-link active" aria-current="page" href="#">Home</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Link</a>
+					<a class="nav-link" href="#">Features</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+					<a class="nav-link" href="#">About Us</a>
 				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="#">Action</a></li>
-						<li><a class="dropdown-item" href="#">Another action</a></li>
-						<li><a class="dropdown-item" href="#">Something else here</a></li>
-					</ul>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact Us</a>
 				</li>
 			</ul>
-			<form role="search">
-				<input class="form-control" type="search" placeholder="Search" aria-label="Search">
-			</form>
+			<button type="button" class="btn default_btn px-4 py-2">Join the club</button>
 		</div>
 	</div>
 </nav>
