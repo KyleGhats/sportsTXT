688ae254f16401b86d5810678a7a9f0a261091b9
Kyle Gatudan <101082963+KyleGhats@users.noreply.github.com>
2024-05-03 11:39:00 +0800

    Delete dist directory

--- dist/layouts/styles.php
+++
@@ -1,1 +0,0 @@
-<?php if ($in_concat == true) { ?> <link rel="stylesheet" type="text/css" href="./assets/css/style.min.css"> <?php } else { ?> <link rel="stylesheet" type="text/css" href="../assets/css/style.min.css"> <?php } ?>
